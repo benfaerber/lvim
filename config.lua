c27368d65271cd72f767fbf8aff912c129dcb697
Ben Faerber <benfaerber@protonmail.com>
2023-11-18 15:26:20 -0700

    Remove comment

--- config.lua
+++ config.lua
@@ -207,10 +207,6 @@ vim.diagnostic.config({
 
 vim.cmd([[set relativenumber]])
 
--- vim.diagnostic.config({
---   virtual_text = true
--- })
-
 -- -- Show line diagnostics automatically in hover window
 -- vim.o.updatetime = 250
 -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
