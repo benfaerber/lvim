efa4b29655a095682e4f7b01f5415c1dd9f402d1
Ben Faerber <benfaerber@protonmail.com>
2023-12-14 11:52:42 -0700

    Rest

--- config.lua
+++ config.lua
@@ -21,6 +21,15 @@ lvim.plugins = {
     "p00f/nvim-ts-rainbow",
     "axelvc/template-string.nvim",
     "almo7aya/openingh.nvim",
+    {
+        "rest-nvim/rest.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+             require("rest-nvim").setup({
+               --- Get the same options from Packer setup
+            })
+          end
+    }
 }
 
 lvim.colorscheme = "one_monokai"
@@ -201,6 +210,7 @@ vim.diagnostic.config({
 
 vim.cmd([[set relativenumber]])
 
+-- happy_camper_dot_com
 -- Make _ count as a word seperator
 vim.cmd([[set iskeyword-=_]])
 vim.cmd([[set ic]])
