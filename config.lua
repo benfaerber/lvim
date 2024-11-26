baea78b9e5c87bc5bb1137bfb88fd399019349ad
Ben Faerber <benfaerber@protonmail.com>
2024-11-26 08:12:04 -0700

    Project Root

--- config.lua
+++ config.lua
@@ -4,6 +4,10 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
 -- Master for life!
+-- I will never be a cuck and use main!
+
+-- I save the folder that was initially opened because its most likely a project root
+local project_root = vim.fn.expand("%")
 
 lvim.plugins = {
     "cpea2506/one_monokai.nvim",
@@ -223,10 +227,9 @@ lvim.builtin.which_key.mappings.g.w = { ":OpenInGHFile<CR>", "Open on GitHub" }
 -- This opens the current file (respecting line and column) in VSCode
 local open_in_vscode = function ()
     local filepath = vim.fn.expand("%")
-    local dir = filepath:match("(.+)/[^/]+$")
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     local goto_point = filepath .. ":" .. line .. ":" .. col
-    os.execute("code \"" .. dir .. "\" --new-window --goto \"" .. goto_point .. "\"")
+    os.execute("code \"" .. project_root .. "\" --new-window --goto \"" .. goto_point .. "\"")
 end
 
 local open_in_nautilus = function ()
